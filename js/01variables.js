76feb563a549bc836d651d28f27dd5624048667b
k3r1x1k <kirilshostak2020@gmail.com>
2026-02-03 23:16:39 +0100

    зробив все що я знаю

---
+++ js/01variables.js
@@ -0,0 +1,19 @@
+const myAge = 14;
+console.log (myAge);
+
+const myName = "Kiril";
+console.log (myName);
+
+const isStudent = "false";
+console.log (myName);
+
+const myString = "«Ах, люблю я кавун. Навіть...»";
+console.log (myName);
+
+let myNumber  = 8;
+console.log (myNumber);
+myNumber  = 8+10;
+console.log (myNumber);
+
+const myNull "null";
+console.log (myNull);
