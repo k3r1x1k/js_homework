7246e8b5e75995606bac10483be341d2b065b137
k3r1x1k <kirilshostak2020@gmail.com>
2026-03-03 13:35:14 +0100

    02

---
+++ js/02variables.js
@@ -0,0 +1,45 @@
+const celsius = 25;
+const fahrenheit = celsius * 9 / 5 + 32;
+console.log(fahrenheit);
+
+const daysInMonth = 30;
+const hoursInMonth = daysInMonth * 24;
+const minutesInMonth = hoursInMonth * 60;
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let health = 100;
+let energy = 80;
+health === health - 20;
+energy === energy - 30;
+console.log(health);
+console.log(energy);
+
+const totalPrice = 1000;
+const discount = 0.10;
+const discountedPrice = totalPrice * (1 - discount);
+console.log(discountedPrice);
+
+const floatNumber = 12.78;
+const roundedDown = Math.floor(floatNumber);
+console.log(roundedDown);
+
+const floatString = "45.67";
+const parsedFloat = parseFloat(floatString);
+console.log(parsedFloat);
+
+const intString = "1234";
+const parsedInt = parseInt(intString);
+console.log(parsedInt);
+
+const number = 64;
+const sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber);
+
+const integer = 42;
+const stringNumber = "256";
+const convertedInt = parseInt(stringNumber);
+console.log(convertedInt);
+
+const convertedString = integer.toString();
+console.log(convertedString);
