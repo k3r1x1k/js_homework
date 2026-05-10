5e54f265afac808a2b5ab4c677539ca1e49f709b
k3r1x1k <kirilshostak2020@gmail.com>
2026-05-10 18:36:23 +0200

    05

---
+++ js/04variables.js
@@ -0,0 +1,8 @@
+const field1 = "Hello";
+const field2 = "World";
+
+if (field1 !== "" && field2 ==! "") {
+  console.log("Обидва поля заповнені");
+} else {
+  console.log("Не всі поля заповнені");
+}
